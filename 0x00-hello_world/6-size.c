cc92af4de5efbd383c7eab3e1896057ef0b7c30a
Billie-K <billiemush@gmail.com>
2021-06-17 07:26:18 -0700

    Type size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 int main(void)
 {
 int a = 16;
- printf("Size of a char: %d\n", sizeof(char), " byte(s)");
- printf("Size of an int: %d\n", sizeof(int), " byte(s)");
- printf("Size of a long int: %d\n", sizeof(long int), " byte(s)");
- printf("Size of a long long int : %d\n", sizeof(long long int), " bytes(s)");
- printf("Size of a float : %d\n", sizeof(float), " byte(s)");
+printf("Size of a char: %d", sizeof(char), " byte(s)");
+printf("Size of an int: %d", sizeof(int), " byte(s)");
+printf("Size of a long int: %d", sizeof(long int), " byte(s)");
+printf("Size of a long long int : %d", sizeof(long long int), " bytes(s)");
+printf("Size of a float : %d", sizeof(float), " byte(s)");
 return (0);
 }
