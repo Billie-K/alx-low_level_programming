0505c37e07a25f7c056a94174912ace25d44ece6
Billie-K <billiemush@gmail.com>
2021-06-18 05:10:09 -0700

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+* main - the main func
+*
+* Description: a program that prints base
+* 10 numbers
+* Return: returns the numbers
+*/
+
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+}
+putchar('\n');
+return (0);
+}
