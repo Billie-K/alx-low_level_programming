282c13a81cdc5b57b71d8559e3a1dcb658ae5f42
Billie-K <billiemush@gmail.com>
2021-06-18 04:26:44 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+* main - the main func
+*
+* Description: a program that prints base
+* 10 numbers
+* Return: returns the numbers
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
