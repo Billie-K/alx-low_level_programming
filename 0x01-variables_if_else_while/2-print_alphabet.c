cc60010a8edd4b66d6ffaa0c2179470dd4f173cc
Billie-K <billiemush@gmail.com>
2021-06-18 05:44:44 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - the main func
+*
+* Description: an alphabet program
+* Return: return 0
+*/
+int main(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+{
+putchar('\n');
+}
+
+return (0);
+}
