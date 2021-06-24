e0f1a90bcb0a40d9303ae31e1e2593ddb0fcc4dd
Billie-K <billiemush@gmail.com>
2021-06-24 12:00:52 -0700

    Skip 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* print_most_numbers - print nums to output
+*
+* Return: nothing
+*/
+
+void print_most_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+if (i == 50 || i == 52)
+{
+continue;
+}
+_putchar(i);
+}
+_putchar('\n');
+}
