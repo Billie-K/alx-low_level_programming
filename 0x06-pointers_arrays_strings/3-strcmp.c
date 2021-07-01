5a2798cbdb64f769edfe67498d5d7a5a5e2d17ff
Billie-K <billiemush@gmail.com>
2021-07-01 07:28:18 -0700

    Compare two str

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+/**
+* _strcmp - compare two strings
+* @s1: string to be compared
+* @s2: string to compare with
+*
+* Return: the difference
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i;
+
+for (i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
+;
+return (s1[i] - s2[i]);
+}
