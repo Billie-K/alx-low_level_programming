fe20de45067c4fb32a83c0ee212ccb80d15e84dc
Billie-K <billiemush@gmail.com>
2021-07-01 07:14:52 -0700

    Str cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,8 @@
 /**
 * *_strcat - concatenates two strings
 * @dest: the first string
-* @src: the second string 
-* 
+* @src: the second string
+*
 * Return: concatenation of the strings
 */
 
@@ -12,12 +12,15 @@ char *_strcat(char *dest, char *src)
 int i, j;
 
 i = 0;
+
 for (j = 0; dest[j] != '\0'; j++)
-		;
+;
 while (src[i] != '\0')
 {
 dest[j + i] = src[i];
 i++;
 }
+
 return (dest);
+
 }
