2e15b5cc05a612687a541f0cffc228011413898e
Billie-K <billiemush@gmail.com>
2021-07-07 11:21:09 -0700

    pow raise

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* _pow_recursion - return x raised to the power
+0;136;0c* of y
+* @x: the first parameter
+* @y: the second parameter
+* Return: x raised to y
+*/
+int _pow_recursion(int x, int y)
+{
+int res = x;
+
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+
+res *= _pow_recursion(x, y-1);
+
+return (res);
+}
