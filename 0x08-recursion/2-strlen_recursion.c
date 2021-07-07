af4cb00b15de70d3439468a019106862a0bd53c3
Billie-K <billiemush@gmail.com>
2021-07-07 10:46:12 -0700

    str length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* _strlen_recursion - returns the length of
+* a string
+* @s: the string parameter
+*/
+int _strlen_recursion(char *s)
+{
+int count = 0;
+if (*s)
+{
+count++;
+count += _strlen_recursion(s + 1);
+}
+return (count);
+}
